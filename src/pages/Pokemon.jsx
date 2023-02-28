3174c74b630cdf55d5e982d01e92c2b6d5f9e88b
Carlos Eduardo <cpwaldow@gmail.com>
2023-02-27 22:49:29 -0300

    fetch com os primeiros 50 pokemons e renderizando o nome em tela

--- src/pages/Pokemon.jsx
+++ src/pages/Pokemon.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import pokeApi from '../services/pokeApi';
 
 const Pokemon = () => {
-  return <div>Pokemon</div>;
+  const [pokemons, setPokemons] = React.useState();
+  React.useEffect(() => {
+    pokeApi
+      .get('?limit=50&offset=0')
+      .then((response) => setPokemons(response.data.results))
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }, []);
+
+  if (!pokemons) return null;
+
+  return (
+    <div>
+      <section>
+        {pokemons.map((pokemon) => {
+          return <h3 key={pokemon.name}>{pokemon.name}</h3>;
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default Pokemon;
